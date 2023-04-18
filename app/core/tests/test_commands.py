1199f531d1474674852308601e871e9452ab8b96
IAmJustAmateur <ig.hizhnyak@outlook.com>
2023-04-18 17:39:53 +0300

    some minor changes in naming

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,7 +3,7 @@ Test django management command
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -11,10 +11,10 @@ from django.test import SimpleTestCase
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class CommandsTest(SimpleTestCase):
+class CommandTests(SimpleTestCase):
     """Test commands."""
 
-    def test_for_db_ready(self, patched_check):
+    def test_wait_for_db_ready(self, patched_check):
         """Test for database ready if database ready"""
         patched_check.return_value = True
 
@@ -23,9 +23,9 @@ class CommandsTest(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_for_db_delay(self, patched_sleep, patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError"""
-        patched_check.side_effects = [Psycopg2Error] * 2 + \
+        patched_check.side_effects = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
         
         call_command('wait_for_db')
