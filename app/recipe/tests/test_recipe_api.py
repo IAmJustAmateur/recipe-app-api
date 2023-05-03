d35c0476551159c419024e51966270d79e8429fd
IAmJustAmateur <ig.hizhnyak@outlook.com>
2023-05-03 12:44:14 +0300

    API image implemented"

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -385,7 +385,7 @@ class ImageUploadTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.user = get_user_model.objects.create_user(
+        self.user = get_user_model().objects.create_user(
             'user@example.com',
             'password123',
         )
